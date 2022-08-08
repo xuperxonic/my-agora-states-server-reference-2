29fd9c55d20487ce98c8a24b152f90856a0ed2fc
xuperxonic <xuperxonic12@gmail.com>
2022-08-08 17:45:23 +0900

    App.test.js 파일 생성 및 test-practice.yml 파일 수정

---
+++ my-agora-states-client/src/App.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App test', () => {
+    it('should render App', () => {
+        render(<App />);
+    });
+});
